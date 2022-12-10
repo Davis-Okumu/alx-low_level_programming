189bd60157511e205796b2ce5fe1737c4c0136ce
Davis-Okumu <davisokumu12@gmail.com>
2022-12-10 03:29:43 -0800

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
 
 /**
-
  * main - Entry point
-
- *
-
  * Return: Always 0 (Success)
-
  */
 int main(void)
 {
-        char myChar[]="\"Programming is like building a multilingual puzzle";
-
+	char myChar[] = "\"Programming is like building a multilingual puzzle";
 	puts(myChar);
-
 	return (0);
-
 }
