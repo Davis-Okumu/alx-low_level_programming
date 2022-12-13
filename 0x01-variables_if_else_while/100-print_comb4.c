9d1d76f1fee2289143bda0302525b45ad428f339
Davis-Okumu <davisokumu12@gmail.com>
2022-12-13 14:53:32 -0800

    done

---
+++ 0x01-variables_if_else_while/100-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (i = 0; i < 10; i++)
+			{
+				if ((k < j) && (j < i))
+				{
+					putchar(k + '0');
+					putchar(j + '0');
+					putchar(i + '0');
+					if (k != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
