bbc9356f1e38f45e4f7ce9b10aa2bb3f3ea8a76e
Davis-Okumu <davisokumu12@gmail.com>
2022-12-13 10:19:40 -0800

    Done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *Return: always o (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
