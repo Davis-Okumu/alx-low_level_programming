a363c4b850ed70ad50b70f4b4c639b4172a4b62c
Davis-Okumu <davisokumu12@gmail.com>
2022-12-13 12:18:42 -0800

    Done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - main enetry
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <=9; n++)
+	{
+		putchar(n);
+		putchar(',');
+	}
+	return (0);
+}
