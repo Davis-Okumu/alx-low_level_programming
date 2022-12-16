763ac4414216674ebeef37e0bf80fe00e93dc3b7
Davis-Okumu <davisokumu12@gmail.com>
2022-12-16 05:32:52 -0800

    Done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul(int a, int b) - Entry point
+ *Description:multiplies two integers
+ *Return: int value
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a*b;
+	return (product);
+}
