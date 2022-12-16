e41e0b9c03e2e1a4bc292650d66b94d8580b6224
Davis-Okumu <davisokumu12@gmail.com>
2022-12-16 06:53:03 -0800

    Done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,22 @@
  */
 void more_numbers(void)
 {
-	int n;
+	int n, i;
 
 	for (n = 0; n <= 10; n++)
 	{
 		int ch;
 
-		for (ch = "0"; ch <= "14"; ch++)
+		for (ch = 0; ch <= 14; ch++)
 		{
-			_putchar(ch);
+			i = ch;
+			if (ch > 9)
+			{
+				_putchar(1 + '0');
+				i = ch % 10;
+			}
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
