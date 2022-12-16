9e6f304c7e736cedf1ab9f240b545e8679ba0d53
Davis-Okumu <davisokumu12@gmail.com>
2022-12-16 09:48:09 -0800

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,6 @@
  *_isupper - Entry point
  *Description: Checks for uppercase letters
  *Return:Always 0 (success)
- *Return: 1 if c is uppercase(true), otherwise 0 (false)
  */
 int _isupper(int c)
 {
