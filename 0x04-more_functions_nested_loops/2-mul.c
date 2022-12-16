e81bd8e7bb32869555ff8ed175e121817b0b3642
Davis-Okumu <davisokumu12@gmail.com>
2022-12-16 15:36:17 -0800

    Done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *mul - Entry point
- *Description: multiplies two integers
- *Return: Always 0 (success)
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: product (a * b)
  */
 int mul(int a, int b)
 {
