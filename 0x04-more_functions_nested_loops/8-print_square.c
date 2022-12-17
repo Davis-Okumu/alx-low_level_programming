09708f29ab73a5e9c41dd91afdba56fad0783b19
Davis-Okumu <davisokumu12@gmail.com>
2022-12-16 17:10:28 -0800

    Done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,18 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
+	int i, j;
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		int i, j;
-
-		for (i = 0; i < size; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 }
