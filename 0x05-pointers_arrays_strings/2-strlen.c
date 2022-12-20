871b2c3f88ae77e0e22caf3d92f974474857caeb
Davis-Okumu <davisokumu12@gmail.com>
2022-12-19 18:32:55 -0800

    Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strlen - returns the length of a string
@@ -7,9 +8,8 @@
  */
 int _strlen(char *s)
 {
-	char *s;
 	int n;
 
-	n = sizeof(s);
+	n = strlen(s);
 	return (n);
 }
