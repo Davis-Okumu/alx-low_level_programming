91ba53eb6d58505fb7ca2ae08c2b91780036be0e
Davis-Okumu <davisokumu12@gmail.com>
2022-12-21 14:46:08 -0800

    Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to be returned
- * return: the length of a string
+ * return:integer,the length of a string
  */
 int _strlen(char *s)
 {
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,9 +8,12 @@
  */
 void _puts(char *str)
 {
-	for(str = 0; strlen(str) != '\0'; str++)
+	int i = 0;
+
+	while (str[i])
 	{
-		_putchar(*str);
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
