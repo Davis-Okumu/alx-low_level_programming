96e7882da95100473773a2651a760677cb42ab49
Davis-Okumu <davisokumu12@gmail.com>
2023-02-07 14:09:25 -0800

    locates a charachter in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strchr - function that locates a character in a sting
+ * _strchr - function that locates a character in a string
  * @s: memory area that points to s
  * @c: character in a string
  * Return: A charchter or Null
