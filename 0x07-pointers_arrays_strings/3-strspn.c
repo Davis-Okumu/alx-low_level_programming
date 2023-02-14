5769ec7adf8d3cccc2db04a27488686f0de90a08
Davis-Okumu <davisokumu12@gmail.com>
2023-02-14 05:50:03 -0800

    gets the length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - get the lenght of the prefix substring
+ * @s: String to search in
+ * @accept: charchters to search for
+ * Return: numbers of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
