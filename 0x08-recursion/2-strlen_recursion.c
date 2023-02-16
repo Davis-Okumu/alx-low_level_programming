48e0554d315013be67084ed595418510b581e0ff
Davis-Okumu <davisokumu12@gmail.com>
2023-02-16 05:32:15 -0800

    Prints lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ * @s: string whose lenght is retuned.
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (strlen(s))
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
