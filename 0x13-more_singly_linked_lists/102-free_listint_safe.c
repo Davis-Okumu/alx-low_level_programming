064f3507e83031a79b357a4e926bc0e672874b18
Davis-Okumu <davisokumu12@gmail.com>
2023-02-23 05:05:41 -0800

    More singly linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,14 +9,14 @@
 void free_listp2(listp_t **head)
 {
 	listp_t *temp;
-	listp_t *nrn;
+	listp_t *curr;
 
 	if (head != NULL)
 	{
-		nrn = *head;
+		curr = *head;
 		while ((temp = curr) != NULL)
 		{
-			nrn = nrn->next;
+			curr = curr->next;
 			free(temp);
 		}
 		*head = NULL;
@@ -26,13 +26,14 @@ void free_listp2(listp_t **head)
 /**
  * free_listint_safe - frees a linked list.
  * @h: head of a list.
+ *
  * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
-	listint_t *nrn;
+	listint_t *curr;
 
 	hptr = NULL;
 	while (*h != NULL)
@@ -40,13 +41,14 @@ size_t free_listint_safe(listint_t **h)
 		new = malloc(sizeof(listp_t));
 
 		if (new == NULL)
-		{
 			exit(98);
-		}
+
 		new->p = (void *)*h;
 		new->next = hptr;
 		hptr = new;
+
 		add = hptr;
+
 		while (add->next != NULL)
 		{
 			add = add->next;
@@ -57,11 +59,13 @@ size_t free_listint_safe(listint_t **h)
 				return (nnodes);
 			}
 		}
-		nrn = *h;
+
+		curr = *h;
 		*h = (*h)->next;
-		free(nrn);
+		free(curr);
 		nnodes++;
 	}
+
 	*h = NULL;
 	free_listp2(&hptr);
 	return (nnodes);
